81514fe3ac21cf8436118ff10f29b9750f7cbfb0
tmohammad78 <36788573+tmohammad78@users.noreply.github.com>
2019-02-06 12:00:19 -0800

    Add files via upload
    its contact us page for a start up with bootstrap 4

---
+++ script.js
@@ -0,0 +1,106 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input3').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      });
+      
+      // Initialize popover component
+	$(window).bind('scroll', function() {
+		var navHeight = $('.main-header').height();
+		if ($(window).scrollTop() > navHeight) {
+			$('.nav-bar').addClass('fixed');
+			$('.nav-1').addClass('toleft');
+		 }
+		else {
+			$('.nav-bar').removeClass('fixed');
+			$('.nav-1').removeClass('toleft');
+		 }
+	});
+
+
+    /*==================================================================
+    [ Chose Radio ]*/
+    $("#radio1").on('change', function(){
+        if ($(this).is(":checked")) {
+            $('.input3-select').slideUp(300);
+        }
+    });
+
+    $("#radio2").on('change', function(){
+        if ($(this).is(":checked")) {
+            $('.input3-select').slideDown(300);
+        }
+    });
+        
+  
+    
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input3').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
+
+function newFunction() {
+    return 'scrolled-nav';
+}
